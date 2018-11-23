7f6684f0a698688231de0fc2a893e8f02e742173
BALKANGraph <plamen.peshev@live.com>
2018-11-23 21:40:00 +0200

    Added zoom

---
+++ OrgChart/Zoom/demo.js
@@ -0,0 +1,25 @@
+window.onload = function () {
+    var chart = new OrgChart(document.getElementById("tree"), {
+        template: "polina",
+        enableDelete: true,
+        enableEdit: true,
+        enableAdd: true,
+        enableSearch: false,
+        scaleInitial: BALKANGraph.match.boundary,
+        mouseScroolBehaviour: BALKANGraph.action.zoom,
+        nodeBinding: {
+            field_0: "name",
+            field_1: "title",
+            img_0: "img"
+        },
+        nodes: [
+            { id: 1, name: "Denny Curtis", title: "CEO", img: "https://balkangraph.com/js/img/2.jpg" },
+            { id: 2, pid: 1, name: "Ashley Barnett", title: "Sales Manager", img: "https://balkangraph.com/js/img/3.jpg" },
+            { id: 3, pid: 1, name: "Caden Ellison", title: "Dev Manager", img: "https://balkangraph.com/js/img/4.jpg" },
+            { id: 4, pid: 2, name: "Elliot Patel", title: "Sales", img: "https://balkangraph.com/js/img/5.jpg" },
+            { id: 5, pid: 2, name: "Lynn Hussain", title: "Sales", img: "https://balkangraph.com/js/img/6.jpg" },
+            { id: 6, pid: 3, name: "Tanner May", title: "Developer", img: "https://balkangraph.com/js/img/7.jpg" },
+            { id: 7, pid: 3, name: "Fran Parsons", title: "Developer", img: "https://balkangraph.com/js/img/8.jpg" }
+        ]
+    });    
+};
---
+++ OrgChart/Zoom/readme.md
@@ -0,0 +1,3 @@
+# Zoom
+The above application scenario is useful in cases when the organizational structure is large and the user needs to closely observe a particular item or group of items. The OrgChart JS zooming feature, added through JavaScript, allows the user to zoom the chart so the data is shown in greater detail.
+
